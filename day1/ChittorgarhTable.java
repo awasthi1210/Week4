5966d9e3449eeb428c11ced510eb152f43488567
awasthi1210 <107309239+awasthi1210@users.noreply.github.com>
2022-07-07 23:07:11 +0530

    Update ChittorgarhTable.java

--- day1/ChittorgarhTable.java
+++ day1/ChittorgarhTable.java
@@ -1,6 +1,10 @@
 package day1;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,8 +21,11 @@ public class ChittorgarhTable {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.chittorgarh.com/");
+		// Click on stock market
 		driver.findElement(By.id("navbtn_stockmarket")).click();
+		// Click on NSE bulk Deals
 		driver.findElement(By.xpath("//a[text()='NSE Bulk Deals']")).click();
+		// Get all the Security names
 		driver.findElement(
 				By.xpath("//h2[contains(text(),'NSE Bulk Deals From Last Trading Session')]//parent::div//table"));
 
@@ -26,18 +33,42 @@ public class ChittorgarhTable {
 		Thread.sleep(2000);
 		List<WebElement> rows = driver.findElements(By.xpath(
 				"//h2[contains(text(),'NSE Bulk Deals From Last Trading Session')]//parent::div//table/tbody/tr"));
-		printAllRowForAColumn(rows,2);
-		
-
-		
+		printAllRowForAColumn(rows, 2);
+		// Ensure whether there are duplicate Security names
+		List<String> colDataInList = appendColumnDataToAList(rows, 2);
+		System.out.println("the list contains duplicate :  " + isListContainsDuplicate(colDataInList));
 
 	}
 
-	
-	public static void printAllRowForAColumn(List<WebElement>rows,int colIndex) {
-			for(WebElement row: rows) {
-				List<WebElement> cols = row.findElements(By.xpath(".//td"));
-				System.out.println(cols.get(colIndex).getText());
-			}
+// Create a generic method to print the particular column data
+
+	public static void printAllRowForAColumn(List<WebElement> rows, int colIndex) {
+		for (WebElement row : rows) {
+			List<WebElement> cols = row.findElements(By.xpath(".//td"));
+			String text = cols.get(colIndex).getText();
+			System.out.println(text);
+		}
+	}
+
+	public static List<String> appendColumnDataToAList(List<WebElement> rows, int colIndex) {
+		List<String> columnData = new ArrayList<String>();
+		for (WebElement row : rows) {
+			List<WebElement> cols = row.findElements(By.xpath(".//td"));
+			String text = cols.get(colIndex).getText();
+			columnData.add(text);
+		}
+		return columnData;
+
+	}
+
+	public static boolean isListContainsDuplicate(List<String> listToCheck) {
+		Set<String> set = new TreeSet<String>(listToCheck);
+		System.out.println(set);
+		if (set.equals(listToCheck)) {
+			return false;
+		} else {
+			return true;
+		}
+
 	}
 }
